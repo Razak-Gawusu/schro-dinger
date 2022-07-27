70661ed09c82eeaab2d9beae94e4b3418100cc53
Razak-Gawusu <gawusur@gmail.com>
2022-07-27 23:25:49 +0100

    fixed style

--- src/components/Projects/ProjectMainInfo.jsx
+++ src/components/Projects/ProjectMainInfo.jsx
@@ -5,36 +5,35 @@ function ProjectMainInfo() {
     <div className='projectMainInfo'>
         <div className="projectMainInfo__item">
           <img src={require('../../images/mooncoin.png')} alt="mooncoin" />
-        </div>
-
-        <div className="projectMainInfo__item">
-          <h3 className='title'>Moon Token</h3>
-          <div className='details'>
-            <div className='details__info'>
-              <img src={require('../../images/etherscan-logo-circle.png')} alt="ether-icon" />
-              Etherscans
-            </div>
-            <div className='details__info'>
-              <img src={require('../../images/web.png')} alt="ether-icon" />
-              Website
-            </div>
-            <div className='details__info'>
-              <img src={require('../../images/link.png')} alt="ether-icon" />
-              Address
-            </div>
-            <div className='details__info'>
-              <img src={require('../../images/telegram2382.png')} alt="ether-icon" />
-              Telegram
-            </div>
-            <div className='details__info'>
-              <img src={require('../../images/discord7805.png')} alt="ether-icon" />
-              Discord
-            </div>
-            <div className='details__info'>
-              <img src={require('../../images/twitter7806.png')} alt="ether-icon" />
-              Twitter
-            </div>
+          <div className="info">
+            <h3 className='title'>Moon Token</h3>
+            <div className='details'>
+              <div className='details__info'>
+                <img src={require('../../images/etherscan-logo-circle.png')} alt="ether-icon" />
+                Etherscans
+              </div>
+              <div className='details__info'>
+                <img src={require('../../images/web.png')} alt="ether-icon" />
+                Website
+              </div>
+              <div className='details__info'>
+                <img src={require('../../images/link.png')} alt="ether-icon" />
+                Address
+              </div>
+              <div className='details__info'>
+                <img src={require('../../images/telegram2382.png')} alt="ether-icon" />
+                Telegram
+              </div>
+              <div className='details__info'>
+                <img src={require('../../images/discord7805.png')} alt="ether-icon" />
+                Discord
+              </div>
+              <div className='details__info'>
+                <img src={require('../../images/twitter7806.png')} alt="ether-icon" />
+                Twitter
+              </div>
           </div>
+        </div>
         </div>
 
         <div className="projectMainInfo__item">
